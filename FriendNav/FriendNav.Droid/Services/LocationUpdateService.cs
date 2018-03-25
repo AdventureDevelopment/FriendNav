2d435cf4dfd3628e4061b00400d74201ea444203
Yao sun <yao.sun@artesyn.com>
2018-03-25 11:31:24 -0500

    Initial changes to location update services

---
+++ FriendNav/FriendNav.Droid/Services/LocationUpdateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using FriendNav.Core.Services.Interfaces;
+
+namespace FriendNav.Droid.Services
+{
+    public class LocationUpdateService: ILocationUpdateService
+    {
+        Android.Gms.Location.FusedLocationProviderClient fusedLocationProviderClient;
+
+        public string getInitiatorLatitue() {
+            string returnValue = "200";
+            return returnValue;
+        }
+    }
+}
