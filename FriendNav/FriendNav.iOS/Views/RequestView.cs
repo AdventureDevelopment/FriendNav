ba2af5e95a68296a8d92a8fd5b3b4254d0a90079
pietore <ungericw@tutanota.com>
2018-04-29 17:38:48 -0500

    Request View navigation

--- FriendNav/FriendNav.iOS/Views/RequestView.cs
+++ FriendNav/FriendNav.iOS/Views/RequestView.cs
@@ -3,17 +3,15 @@ using System;
 using System.Drawing;
 
 using Foundation;
-using FriendNav.Core.ViewModels;
-using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
 using UIKit;
 
 namespace FriendNav.iOS.Views
 {
     [MvxFromStoryboard]
-    public partial class ChatView : MvxViewController
+    public partial class RequestView : MvxViewController
     {
-        public ChatView(IntPtr handle) : base(handle)
+        public RequestView(IntPtr handle) : base(handle)
         {
         }
 
@@ -22,9 +20,6 @@ namespace FriendNav.iOS.Views
             base.ViewDidLoad();
 
             // Perform any additional setup after loading the view, typically from a nib.
-            var set = this.CreateBindingSet<ChatView, ChatViewModel>();
-            set.Bind(ChatRequestButton).To(vm => vm.SendNavigationRequestCommand);
-            set.Apply();
         }
     }
 }
