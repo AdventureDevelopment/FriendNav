063e5ed13d1c204714e2aed590e254d8f714237f
Yao Sun <Yao.Sun.17@gmail.com>
2018-05-13 11:16:17 -0500

    Adding the code for adding repository to firebase

--- FriendNav/FriendNav.Core/Repositories/MapRepository.cs
+++ FriendNav/FriendNav.Core/Repositories/MapRepository.cs
@@ -68,9 +68,21 @@ namespace FriendNav.Core.Repositories
             return map;
         }
 
-        public void UpdateMap(Map map)
+        public async Task UpdateMap(Map map)
         {
+            var client = _firebaseClientService.CreateFirebaseClient();
 
+            await client
+            .Child("BasicChat")
+            .Child(map.ChatFirebaseKey)
+            .Child("meetRequest")
+            .PutAsync(new MapDto
+            {
+                InitiatorLatitude = map.InitiatorLatitude,
+                InitiatorLongitude = map.InitiatorLatitude,
+                ResponderLatitude = map.ResponderLatitude,
+                ResponderLongitude = map.ResponderLongitude
+            });
         }
 
         public void Dispose()
